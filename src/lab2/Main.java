7699f88434be6e486d5b28ab0a878e22df57a187
Sergey Koveleuski <serkov2345@gmail.com>
2018-10-16 13:06:05 +0300

    v.1.0.1. (improoved output)

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -4,11 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private static boolean isDigitOrDot(char c){
-        return Character.isDigit(c)||(c=='.');
+    private static boolean isDigitOrDot(char c) {
+        return Character.isDigit(c) || (c == '.');
     }
+
     private static String parse(String str) {
-        StringBuilder strBuf = new StringBuilder('#'+str);
+        StringBuilder strBuf = new StringBuilder('#' + str);
         for (int i = 0; i < strBuf.length() - 2; i++) {
             if (strBuf.charAt(i + 1) == '0' && !isDigitOrDot(strBuf.charAt(i)) && Character.isDigit(strBuf.charAt(i + 2))) {
                 strBuf.deleteCharAt(i + 1);
@@ -17,7 +18,8 @@ public class Main {
         }
         return strBuf.substring(1);
     }
-//    private static String parse1(String str){
+
+    //    private static String parse1(String str){
 //        StringBuilder stringBuilder=new StringBuilder(str);
 //        Pattern pattern=Pattern.compile("((0+)\\d+\\.?\\d*)");
 //        Matcher  m = pattern.matcher(stringBuilder);
@@ -31,7 +33,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             System.out.println("N " + i + " = \"" + args[i] + '\"');
-            System.out.println(parse(args[i]));
+            System.out.println("Answer: \"" + parse(args[i]) + '\"');
         }
     }
 }
